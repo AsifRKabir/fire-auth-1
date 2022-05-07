ca58424077896dbcc66d155ece834292b6610ac1
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2022-05-07 20:45:36 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+initializeApp(firebaseConfig);
 
 function App() {
+
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+
+  const handleSignIn = () => {
+
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+        // ...
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <button onClick={handleSignIn}>Sign In</button>
+
     </div>
   );
 }
